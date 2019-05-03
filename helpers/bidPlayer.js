e3cd300e1c9765dbf1d74363964530280d8e7d65
rpernicone <roberto.pernicone@gmail.com>
2019-05-02 20:50:20 -0500

    deleted playercontroler

--- helpers/bidPlayer.js
+++
@@ -1,5 +0,0 @@
-const Campaign = require('../models/Campaign')
-
-exports.update = (e) => {
-  return Campaign.findOneAndUpdate(e._id, {$inc: {playsPerHour: -1}},{new: true});
-}
--- routes/player.js
+++ routes/player.js
@@ -2,7 +2,6 @@ const express = require("express");
 const server = require("http").Server(express);
 const io = require("socket.io")(server);
 const Campaign = require("../models/Campaign");
-const playControler = require("../helpers/bidPlayer");
 const router = express.Router();
 const Zone = require("../models/Zone");
 
